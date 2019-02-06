7bd49ff2ec8e4f0547d1f36d4af6a62b9f8fdd0a
Cj-bc <cj-bc@y-modify.org>
2019-02-06 19:00:06 +0900

    Remove: remove all files to start from scratch

--- config.py
+++
@@ -1,8 +0,0 @@
-#
-#   here are configs for mcUI
-#
-
-TOPDIRECTORY = "~/" # decide top directory to make real in minecraft
-DIR_SHAPE = "default_Rectangular"
-FILE_SHAPE = "default_Rectangular"
-SHAPE_START_FROM = [ 1,-1 ] # first 1 = east, second -1 = north
