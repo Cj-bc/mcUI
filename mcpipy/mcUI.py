caad6d1a0965ab37a67ec219642ec786e9c4bcc1
Cj-bc <cj-bc@y-modify.org>
2019-02-16 01:52:37 +0900

    Change: mcUI> objects will spread to x-z zone

--- mcpipy/mcUI.py
+++ mcpipy/mcUI.py
@@ -18,7 +18,7 @@ import os
 # configs(should be in config.py) {{{
 mergin = 5 # mergin from player position to center of objects' spawing place
 padding = Vec3(3, 0, 0) # padding between each objects
-line_vec = Vec3(0, 3, 0) # vector to define which axis should objects follow
+line_vec = Vec3(0, 0, 3) # vector to define which axis should objects follow
 MAX_OBJECT_PER_LINE = 6 # how much objects could be in one line?
 # }}}
 
