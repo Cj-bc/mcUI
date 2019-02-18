f79217956431f06f997ba5ef4772385d980510cd
Cj-bc <cj-bc@y-modify.org>
2019-02-18 14:32:45 +0900

    Add: mcui> config, __init_

---
+++ mcpipy/mcui/config.py
@@ -0,0 +1,18 @@
+# how can define Vec3 config??
+# list of filetypes and extension list
+filetype_list = {"": "text",
+                 ".txt": "text",
+                 ".md": "markdown",
+                 ".markdown": "markdown",
+                 ".sh": "shellscript",
+                 ".py": "python",
+                 ".rb": "ruby",
+                 ".config": "config",
+                 ".cfg": "config",
+                 ".html": "html",
+                 ".css": "css",
+                 ".js": "javascript",
+                 ".c": "c",
+                 ".cpp": "c++",
+                 ".exe": "wondows executable"
+                }
