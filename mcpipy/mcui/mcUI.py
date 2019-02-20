2431ee7f818bdbff03c48e9dba0f491742b55998
Cj-bc <cj-bc@y-modify.org>
2019-02-20 13:34:55 +0900

    Change: mcUI> Use session to store state; make main loop

--- mcpipy/mcui/mcUI.py
+++ mcpipy/mcui/mcUI.py
@@ -9,6 +9,7 @@
 # (@)   run this script while Minecraft is running.
 
 import os
+import time
 
 import mcpi.minecraft as minecraft
 from mcpi.vec3 import Vec3
@@ -16,18 +17,34 @@ from entry import Pane
 from util import *
 from commands import ls
 from config import margin, padding, line_vec, MAX_OBJECT_PER_LINE
+from entry import Session
 
 
+# Initialize
 mc = minecraft.Minecraft()
 pwd = os.getcwd()
-
 spawn_object_criteria = mc.player.getPos()
+player_allowed_use_command = mc.getPlayerEntityIds()
 spawn_object_direction_criteria = direction(mc.player.getRotation())
 
 mc.postToChat(f'pwd: {pwd}')
 
-pane = Pane(entries=ls(pwd), pos=spawn_object_criteria, face_to=spawn_object_direction_criteria)
-for entry, pos in zip(pane.entries,calc_entries_coordinate(pane, padding, line_vec, MAX_OBJECT_PER_LINE)):
-    entry.savePos(pos)
+the_session = Session()
+the_session.add_pane(Pane(path=pwd, entries=ls(pwd), pos=spawn_object_criteria, face_to=spawn_object_direction_criteria))
 
-write_pane(mc, pane)
+while not the_session.is_end:
+    for entry, pos in zip(the_session.panes[0].entries,
+                          calc_entries_coordinate(the_session.panes[0], padding, line_vec, MAX_OBJECT_PER_LINE)):
+        entry.savePos(pos)
+
+    for pane in the_session.panes:
+        write_pane(mc, pane)
+
+
+    time.sleep(1)
+
+mc.postToChat('removing mcUI...')
+for pane in the_session.panes:
+    remove_pane(mc, pane)
+
+mc.postToChat('Done.')
