498095fb630e2092b133bb5db6d618e7e96944d2
Cj-bc <cj-bc@y-modify.org>
2019-09-07 17:35:51 +0900

    Add: Render> Renderer to render session

---
+++ mcpipy/mcui/Render.py
@@ -0,0 +1,13 @@
+class Renderer:
+    """ a renderer class """
+    minecraft: mc.Minecraft = None
+    session: Session = None
+    prev_session: Session = None
+
+    def __init__(self, _mc: mc.Minecraft, sc: Session) -> None:
+        self.minecraft = _mc
+        self.session = sc
+
+    def render():
+    """ Render Session to Minecraft world. """
+    pass
