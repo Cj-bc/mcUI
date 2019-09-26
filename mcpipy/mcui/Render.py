455164704b2b2d46be09e5b36ff242843d690384
Cj-bc <cj.bc-sd@outlook.jp>
2019-09-27 00:09:03 +0900

    Add: Render> implement Render.render

--- mcpipy/mcui/Render.py
+++ mcpipy/mcui/Render.py
@@ -1,3 +1,6 @@
+from mcUI.entry import Pane, Session
+from copy import deepcopy
+
 class Renderer:
     """ a renderer class """
     minecraft: mc.Minecraft = None
@@ -10,7 +13,30 @@ class Renderer:
 
     def render(self):
     """ Render Session to Minecraft world. """
-    pass
+        plus, minus = diff(self.prev_session, self.session)
+        schemas = schema
+
+        for p in plus + minus:
+            for entry, pos in zip(p.entries,
+                                  calc_entries_coordinate(p, padding, line_vec, MAX_OBJECT_PER_LINE)):
+                entry.sevePos(pos)
+
+        map(lambda p: map(_writeEntry, p.entries), plus)
+        map(lambda p: map(_removeEntry, p.entries), minus)
+        self.prev_session = deepcopy(self.session)
+
+    @staticmethod
+    def _writeEntry(e: Entry) -> None:
+        mc.setBlock(entry.pos.x, entry.pos.y, entry.pos.z, schemas[e.filetype])
+        ent_id = mc.spawnEntity(entity.ARMORSTAND, e.pos,
+                                '{CustomName: ' + e.filename + \
+                                ', CustomNameVisible: true, NoGravity: true, Invisible: true}')
+        e.saveNameEntityId(ent_id)
+
+    @staticmethod
+    def _removeEntry(e: Entry) -> None:
+        mc.setBlock(e.pos.x, e.pos.y, e.pos.z, block.AIR)
+        mc.removEntity(e.nameEntityId)
 
 
 def diff(prev: Sesssion, new: Session) -> Tuple[Pane, Pane]:
